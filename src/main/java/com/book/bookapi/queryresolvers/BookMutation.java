d9ba7c92977cec5ce2467e692cb9bf29b9457df9
dhakre <username@users.noreply.github.com>
2022-05-22 19:11:04 +0200

    spring boot graphql starter

--- src/main/java/com/book/bookapi/queryresolvers/BookMutation.java
+++ src/main/java/com/book/bookapi/queryresolvers/BookMutation.java
@@ -1,24 +1,27 @@
 package com.book.bookapi.queryresolvers;
 
 import com.book.bookapi.model.Book;
+import com.book.bookapi.repository.BookRepository;
 import com.book.bookapi.types.BookInput;
 import com.book.bookapi.types.DeleteBookInput;
 import com.book.bookapi.types.DeletedBook;
 import com.book.bookapi.types.NewBook;
-import com.book.bookapi.repository.BookRepository;
-import graphql.kickstart.tools.GraphQLMutationResolver;
 import lombok.AllArgsConstructor;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 
 import java.util.Optional;
 
-@Component
+@Controller
 @AllArgsConstructor
-public class BookMutation implements GraphQLMutationResolver {
+public class BookMutation {
 
     BookRepository bookRepository;
 
-    public NewBook newBook(BookInput bookInput){
+    @MutationMapping
+    public NewBook newBook(@Argument("input") BookInput bookInput){
         Book book =  new Book();
         book.setName(bookInput.getName());
         book.setPageCount(bookInput.getPageCount());
@@ -26,7 +29,8 @@ public class BookMutation implements GraphQLMutationResolver {
         return new NewBook(book1.getId(),book1.getName(),book1.getPageCount());
     }
 
-    public DeletedBook deleteBook(DeleteBookInput deleteBookInput){
+    @MutationMapping
+    public DeletedBook deleteBook(@Argument("input") DeleteBookInput deleteBookInput){
         Book deleteBook = new Book();
         Optional<Book> findBook =  bookRepository.findById(deleteBookInput.getId());
         if(findBook.isPresent()){
--- src/main/java/com/book/bookapi/queryresolvers/BookQuery.java
+++ src/main/java/com/book/bookapi/queryresolvers/BookQuery.java
@@ -1,32 +1,55 @@
 package com.book.bookapi.queryresolvers;
 
+import com.book.bookapi.enums.ListOrder;
 import com.book.bookapi.model.Author;
 import com.book.bookapi.model.Book;
-import com.book.bookapi.types.BookFilter;
 import com.book.bookapi.repository.AuthorRepository;
 import com.book.bookapi.repository.BookRepository;
-import graphql.kickstart.tools.GraphQLQueryResolver;
+import com.book.bookapi.types.BookFilter;
 import lombok.AllArgsConstructor;
-import org.springframework.stereotype.Component;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.Comparator;
+import java.util.List;
 
 
-@Component
+@Controller
 @AllArgsConstructor
-public class BookQuery implements GraphQLQueryResolver{
+public class BookQuery{
 
     BookRepository bookRepository;
     AuthorRepository authorRepository;
 
+    @QueryMapping
     public Iterable<Book> allBook(){
         return bookRepository.findAll();
     }
 
-    public Book getBookByName(BookFilter bookFilter){
+    @QueryMapping
+    public Book getBookByName(@Argument("filter") BookFilter bookFilter){
         return bookRepository.findBookByName(bookFilter.getName());
     }
 
+    @QueryMapping
     public Iterable<Author> allAuthor(){
         return authorRepository.findAll();
+    }
+
+    @SchemaMapping(typeName="Book", field="author")
+    public Iterable<Author> getAuthor(Book book, @Argument ListOrder order) {
+        List<Author> allAuthors = (List<Author>) authorRepository.findALLByBookId(book.getId());
+        if (order != null) {
+            switch (order) {
+                case ASC:
+                    allAuthors.sort(Comparator.comparingInt(Author::getStarRating));
+                case DESC:
+                    allAuthors.sort(Comparator.comparingInt(Author::getStarRating).reversed());
+            }
+        }
+        return allAuthors;
     }
 
 }
